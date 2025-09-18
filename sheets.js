148a8f97fe74610730014072f52fe1f8a1749284
ryanhihgj-cyber <ryanhihgj@gmail.com>
2025-09-18 08:26:17 -0600

    Create sheets.js

---
+++ sheets.js
@@ -0,0 +1,8 @@
+const { google } = require('googleapis');
+
+async function getSheetData(sheetId, range) {
+    // Placeholder function to fetch data from Google Sheets
+    return [];
+}
+
+module.exports = { getSheetData };
